ac1aecb6c434de6ae866ee255dc714f9a1e11006
김민식 <kmss69052@naver.com>
2025-06-30 02:40:27 +0900

    feat: add spring-ai/stream

---
+++ stream/src/main/java/ai/stream/ChatController.java
@@ -0,0 +1,36 @@
+package ai.stream;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class ChatController {
+
+  private final ChatClient chatClient;
+
+  public ChatController(ChatClient.Builder builder) {
+    this.chatClient = builder
+      .build();
+  }
+
+  @GetMapping("/chat")
+  public String chat(@RequestParam String message) {
+    return chatClient
+      .prompt(message)
+      .call()
+      .content();
+  }
+
+  @GetMapping("/stream")
+  public Flux<String> chatStream(@RequestParam String message) {
+    return chatClient
+      .prompt(message)
+      .stream()
+      .content();
+  }
+
+}
